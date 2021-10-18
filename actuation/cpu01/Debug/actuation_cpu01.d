98f821f9aeda9531b66086a2febee63eb0c61f74
Dylan Gaub <90060896+dgaub5@users.noreply.github.com>
2021-10-18 15:17:33 -0600

    Adding the actuation code

---
+++ actuation/cpu01/Debug/actuation_cpu01.d
@@ -0,0 +1,195 @@
+# FIXED
+
+actuation_cpu01.obj: ../actuation_cpu01.c
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F28x_Project.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Cla_typedefs.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_device.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/assert.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/_ti_config.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/linkage.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stdarg.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/_types.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/cdefs.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/machine/_types.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stdbool.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stddef.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stdint.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/_stdint40.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/stdint.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/machine/_stdint.h
+actuation_cpu01.obj: C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/_stdint.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_adc.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_analogsubsys.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_cla.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_cmpss.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_cputimer.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_dac.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_dcsm.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_dma.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_ecap.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_emif.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_epwm.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_epwm_xbar.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_eqep.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_flash.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_gpio.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_i2c.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_input_xbar.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_ipc.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_mcbsp.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_memconfig.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_nmiintrupt.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_output_xbar.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_piectrl.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_pievect.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_sci.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_sdfm.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_spi.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_sysctrl.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_upp.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_xbar.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_headers/include/F2837xD_xint.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Examples.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_GlobalPrototypes.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_cputimervars.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Cla_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_EPwm_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Adc_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Emif_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Gpio_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_I2c_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Ipc_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Pie_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Dma_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_SysCtrl_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_Upp_defines.h
+actuation_cpu01.obj: C:/Device_support/F2837xD_common/include/F2837xD_defaultisr.h
+
+../actuation_cpu01.c:
+
+C:/Device_support/F2837xD_common/include/F28x_Project.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Cla_typedefs.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_device.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/assert.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/_ti_config.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/linkage.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stdarg.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/_types.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/machine/_types.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stdbool.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stddef.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/stdint.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/_stdint40.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1100/ccs/tools/compiler/ti-cgt-c2000_21.6.0.LTS/include/sys/_stdint.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_adc.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_analogsubsys.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_cla.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_cmpss.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_cputimer.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_dac.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_dcsm.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_dma.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_ecap.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_emif.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_epwm.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_epwm_xbar.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_eqep.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_flash.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_gpio.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_i2c.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_input_xbar.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_ipc.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_mcbsp.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_memconfig.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_nmiintrupt.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_output_xbar.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_piectrl.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_pievect.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_sci.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_sdfm.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_spi.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_sysctrl.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_upp.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_xbar.h:
+
+C:/Device_support/F2837xD_headers/include/F2837xD_xint.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Examples.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_GlobalPrototypes.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_cputimervars.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Cla_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_EPwm_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Adc_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Emif_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Gpio_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_I2c_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Ipc_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Pie_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Dma_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_SysCtrl_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_Upp_defines.h:
+
+C:/Device_support/F2837xD_common/include/F2837xD_defaultisr.h:
+
