0be14fda9023fc476a877f9a3b28427f1e2964ac
Dylan Gaub <90060896+dgaub5@users.noreply.github.com>
2022-01-18 14:34:36 -0700

    Update actuation_cpu01.c
    Adding more comments to code

--- actuation/cpu01/actuation_cpu01.c
+++ actuation/cpu01/actuation_cpu01.c
@@ -52,99 +52,89 @@ Uint16 trigger = 0;                         // The trigger variable helps identi
 // Beginning the main section of code
 void main(void)
 {
-    InitSysCtrl();      // Initialize System Control
-    EALLOW;
-    ClkCfgRegs.PERCLKDIVSEL.bit.EPWMCLKDIV = 1;
-    EDIS;
+    InitSysCtrl();                                  // Initialize System Control
+    EALLOW;                                         // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
+    ClkCfgRegs.PERCLKDIVSEL.bit.EPWMCLKDIV = 1;     // Enable Clock Configure Registers
+    EDIS;                                           // Using EDIS to clear the EALLOW
 
     // Initialize GPIO
     InitGpio();         // Configure default GPIO
     InitEPwm1Gpio();    // Configure EPWM1 GPIO pins
     InitEPwm5Gpio();    // Configure EPWM5 GPIO pins
 
-    EALLOW;
+    EALLOW;             // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
     GpioCtrlRegs.GPADIR.bit.GPIO31 = 1;         // Drives LED LD2 on controlCARD
-    EDIS;
+    EDIS;                                       // Using EDIS to clear the EALLOWs
     GpioDataRegs.GPADAT.bit.GPIO31 = 1;         // Turn off LED
 
-    // Clear all interrupts and initialize PIE vector table
-    DINT;
-    InitPieCtrl();
-    IER = 0x0000;
-    IFR = 0x0000;
-    InitPieVectTable();
-
     // Map ISR functions
-    EALLOW;
-    PieVectTable.ADCA1_INT = &adca1_isr;            // Function for ADCA interrupt 1
-    EDIS;
+    EALLOW;             // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
+    PieVectTable.ADCA1_INT = &adca1_isr;        // Function for ADCA interrupt 1
+    EDIS;                                       // Using EDIS to clear the EALLOW
 
     // Configure the ADC and power it up
-    ConfigureADC();
+    ConfigureADC();     // Write ADC configurations and power up the ADC for both ADC A and ADC C
 
     // Setup the ADC for ePWM triggered conversions on channel 0
-    SetupADCEpwm();
+    SetupADCEpwm();     // Select the channels to convert and end of conversion flag
 
     // Initialize ePWM modules
-    InitEPwm1();
-    InitEPwm2();
-    InitEPwm5();
+    InitEPwm1();        // Initialize ePWM module 1
+    InitEPwm2();        // Initialize ePWM module 2
+    InitEPwm5();        // Initialize ePWM module 5
 
     // Configure DAC registers
     ConfigureDAC();     // Call the ConfigureDAC function
 
-    // Initialize results buffers
+    // Initialize results buffers - Adca and Adcc Results are 0 as long as resultsIndex is less than RESULTS_BUFFER_SIZE
     for(resultsIndex = 0; resultsIndex < RESULTS_BUFFER_SIZE; resultsIndex++)
     {
-        AdcaResults[resultsIndex] = 0;
-        AdccResults[resultsIndex] = 0;
+        AdcaResults[resultsIndex] = 0;      // Set AdcaResults at current index equal to 0
+        AdccResults[resultsIndex] = 0;      // Set AdcaResults at current index equal to 0
     }
-    resultsIndex = 0;
+    resultsIndex = 0;       // Reset the resultsIndex variable back to 0 for the next iteration
 
     // Enable global interrupts and higher priority real-time debug events
     IER |= M_INT1;          // Enable group 1 interrupts
     EINT;                   // Enable Global interrupt INTM
-    ERTM;                   // Enable Global realtime interrupt DBGM
+    ERTM;                   // Enable Global real time interrupt DBGM
 
-    // Enable PIE interrupt
-    PieCtrlRegs.PIEIER1.bit.INTx1 = 1;
+    PieCtrlRegs.PIEIER1.bit.INTx1 = 1;      // Enable PIE interrupt
 
     // Sync ePWM
-    EALLOW;
-    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 1;
+    EALLOW;                                 // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 1;   // Enable CPU System Registers
 
     // Start ePWM
     EPwm2Regs.ETSEL.bit.SOCAEN = 1;             // Enable SOCA
     EPwm2Regs.TBCTL.bit.CTRMODE = 0;            // Un-freeze and enter up-count mode
 
-    do {
+    do {                                        // Infinite do-while loop
         GpioDataRegs.GPADAT.bit.GPIO31 = 0;     // Turn on LED
         DELAY_US(1000 * 500);                   // ON delay
         GpioDataRegs.GPADAT.bit.GPIO31 = 1;     // Turn off LED
         DELAY_US(1000 * 500);                   // OFF delay
-        // Send Load Torque and Duty Cycle to Opal
-        DacaRegs.DACVALS.all = LoadTorque;
-        DacbRegs.DACVALS.all = DutyCycle;
-    } while(1);
+        DacaRegs.DACVALS.all = LoadTorque;      // Send Load Torque to OPAL-RT
+        DacbRegs.DACVALS.all = DutyCycle;       // Send Duty Cycle to OPAL-RT
+    } while(1);                                 // Infinite do-while loop
 }
 
-void ConfigureDAC(void)
+void ConfigureDAC(void)                         // Configure specifc DAC pins needed to utilize communication with I/O
 {
-    EALLOW;
+    EALLOW;                                     // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
     DacaRegs.DACCTL.bit.DACREFSEL = 1;          // Use ADC references (HSEC Pin 09)
     DacaRegs.DACCTL.bit.LOADMODE = 0;           // Load on next SYSCLK
     DacaRegs.DACOUTEN.bit.DACOUTEN = 1;         // Enable DAC
     DacbRegs.DACCTL.bit.DACREFSEL = 1;          // Use ADC references (HSEC Pin 11)
     DacbRegs.DACCTL.bit.LOADMODE = 0;           // Load on next SYSCLK
     DacbRegs.DACOUTEN.bit.DACOUTEN = 1;         // Enable DAC
-    EDIS;
+    EDIS;                                       // Using EDIS to clear the EALLOW
 }
 
 // Write ADC configurations and power up the ADC for both ADC A and ADC C
 void ConfigureADC(void)
 {
-    EALLOW;
-
+    EALLOW;                                     // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
     // ADC-A
     AdcaRegs.ADCCTL2.bit.PRESCALE = 6;          // Set ADCCLK divider to /4
     AdcaRegs.ADCCTL2.bit.RESOLUTION =  0;       // 12-bit resolution
@@ -158,7 +148,7 @@ void ConfigureADC(void)
     AdccRegs.ADCCTL2.bit.SIGNALMODE = 0;        // Single-ended channel conversions (12-bit mode only)
     AdccRegs.ADCCTL1.bit.INTPULSEPOS = 1;       // Set pulse positions to late
     AdccRegs.ADCCTL1.bit.ADCPWDNZ = 1;          // Power up the ADC
-    EDIS;
+    EDIS;                                       // Using EDIS to clear the EALLOW
 
     DELAY_US(1000);                             // Delay for 1ms to allow ADC time to power up
 }
@@ -167,7 +157,7 @@ void ConfigureADC(void)
 void SetupADCEpwm(void)
 {
     // Select the channels to convert and end of conversion flag
-    EALLOW;
+    EALLOW;                                     // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
     AdcaRegs.ADCSOC0CTL.bit.CHSEL = 2;          // SOC0 will convert pin A0 (HSEC Pin 15)
     AdcaRegs.ADCSOC0CTL.bit.ACQPS = 14;         // Sample window is 100 SYSCLK cycles
     AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = 7;        // Trigger on ePWM2 SOCA/C
@@ -179,7 +169,7 @@ void SetupADCEpwm(void)
     AdccRegs.ADCSOC0CTL.bit.CHSEL = 3;          // SOC0 will convert pin C3 (HSEC Pin 33)
     AdccRegs.ADCSOC0CTL.bit.ACQPS = 14;         // Sample window is 100 SYSCLK cycles
     AdccRegs.ADCSOC0CTL.bit.TRIGSEL = 7;        // Trigger on ePWM2 SOCA/C
-    EDIS;
+    EDIS;                                       // Using EDIS to clear the EALLOW
 }
 
 
@@ -192,7 +182,7 @@ void InitEPwm1(void)
    EPwm1Regs.TBPHS.bit.TBPHS = 0x0000;          // Phase is 0
    EPwm1Regs.TBCTR = 0x0000;                    // Clear counter
    EPwm1Regs.TBCTL.bit.HSPCLKDIV = 1;           // Clock ratio to SYSCLKOUT
-   EPwm1Regs.TBCTL.bit.CLKDIV = 0;
+   EPwm1Regs.TBCTL.bit.CLKDIV = 0;              // Clock Div is 0
    EPwm1Regs.TBCTL.bit.SYNCOSEL = 1;            // SYNC output on CTR = 0
 
    // Setup shadow register load on ZERO
@@ -212,7 +202,7 @@ void InitEPwm1(void)
 
 void InitEPwm2(void)
 {
-    EALLOW;
+    EALLOW;         // (Bit 6) — Emulation access enable bit - Enable access to emulation and other protected registers
     // Assumes ePWM clock is already enabled
     EPwm2Regs.TBCTL.bit.CTRMODE = 3;            // Freeze counter
     EPwm2Regs.TBCTL.bit.HSPCLKDIV = 0;          // TBCLK pre-scaler = /1
@@ -221,7 +211,7 @@ void InitEPwm2(void)
     EPwm2Regs.ETSEL.bit.SOCASEL = 2;            // Select SOCA on period match
     EPwm2Regs.ETSEL.bit.SOCAEN = 1;             // Enable SOCA
     EPwm2Regs.ETPS.bit.SOCAPRD = 1;             // Generate pulse on 1st event
-    EDIS;
+    EDIS;                                       // Using EDIS to clear the EALLOW
 }
 
 
@@ -256,13 +246,13 @@ interrupt void adca1_isr(void)
     // Read the ADC result and store in circular buffer
     if (trigger != 0)
     {
-        AdcaResults[resultsIndex] = AdcaResultRegs.ADCRESULT0;
-        AdccResults[resultsIndex++] = AdccResultRegs.ADCRESULT0;
+        AdcaResults[resultsIndex] = AdcaResultRegs.ADCRESULT0;      // Update Adca results buffer
+        AdccResults[resultsIndex++] = AdccResultRegs.ADCRESULT0;    // Update Adcc results buffer
         if(RESULTS_BUFFER_SIZE <= resultsIndex)
         {
-            resultsIndex = 0;
-            pretrig = 0;
-            trigger = 0;
+            resultsIndex = 0;       // Set ResultIndex to 0
+            pretrig = 0;            // Set pretrig to 0
+            trigger = 0;            // Set trigger to 0
 
             // Update PWMs
             EPwm1Regs.TBPRD = period1;
