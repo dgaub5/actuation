88ea72dccbc07955be1322e588a2e8a22fed443b
Dylan Gaub <90060896+dgaub5@users.noreply.github.com>
2022-02-19 19:02:41 -0700

    Add files via upload

---
+++ actuation/cpu01/actuation_test.c
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------------- //
+// Beginning of File
+//
+// File: test_actuation.c
+//
+// File Description:
+// This file contains the Unit Testing for actuation_cpu01.c
+//
+// Last Edit: 02/16/2022
+//
+// ----------------------------------------------------------------------------- //
+
+//#define NDEBUG                    // Use NDEBUG to remove any assert testing in the file
+#include "F28x_Project.h"           // Device Header File and Examples Include File
+#include <stdio.h>                  // Include the header for print statements, etc.
+#include "assert.h"                 // Using the assertion library to conduct tests
+
+// Beginning of the main section of code
+void test(void)
+{
+
+    printf("Testing Here");
+
+}
+
+// ----------------------------------------------------------------------------- //
+// End of file
+// ----------------------------------------------------------------------------- //
